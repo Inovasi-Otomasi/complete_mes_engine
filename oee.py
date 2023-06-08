5831674b7d3ec9e3a77d626a17d2da0990e456c6
goraasep <39854114+goraasep@users.noreply.github.com>
2023-06-08 10:27:25 +0700

    added delta downtime

--- oee.py
+++ oee.py
@@ -3,8 +3,10 @@ import schedule
 from db_func import *
 import json
 
+
 def millis():
     return int(time.time() * 1000)
+
 
 # def check_requirement(sku_code,delta_item_counter=1):
 #     sku=db_fetchone('select * from sku_list where sku_code="%s"'%sku_code)
@@ -30,80 +32,87 @@ def millis():
 #         db_query(sql)
 
 def oee():
-    line=db_fetch('select * from manufacturing_line')
+    line = db_fetch('select * from manufacturing_line')
     for row in line:
-        line_id=row['id']
-        line_name=row['line_name']
-        order_id=row['order_id']
-        batch_id=row['batch_id']
-        lot_number=row['lot_number']
-        sku_code=row['sku_code']
-        remark=row['remark']
-        location=row['location']
+        line_id = row['id']
+        line_name = row['line_name']
+        order_id = row['order_id']
+        batch_id = row['batch_id']
+        lot_number = row['lot_number']
+        sku_code = row['sku_code']
+        remark = row['remark']
+        location = row['location']
         # performance=row['performance']
         # availability=row['availability']
         # quality=row['quality']
-        status=row['status']
-        temp_time=row['temp_time']
-        setup_time=row['setup_time']
-        cycle_time=row['cycle_time']
-        run_time=row['run_time']
-        down_time=row['down_time']
-        standby_time=row['standby_time']
-        ng_count=row['NG_count']
-        item_counter=row['item_counter']
-        prev_item_counter=row['prev_item_counter']
-        acc_standby_time=row['acc_standby_time']
-        acc_setup_time=row['acc_setup_time']
-        small_stop_time=row['small_stop_time']
+        status = row['status']
+        temp_time = row['temp_time']
+        setup_time = row['setup_time']
+        cycle_time = row['cycle_time']
+        run_time = row['run_time']
+        down_time = row['down_time']
+        standby_time = row['standby_time']
+        ng_count = row['NG_count']
+        item_counter = row['item_counter']
+        prev_item_counter = row['prev_item_counter']
+        acc_standby_time = row['acc_standby_time']
+        acc_setup_time = row['acc_setup_time']
+        small_stop_time = row['small_stop_time']
         # order_id=row['order_id']
-        target=row['target']
-        sku_code=row['sku_code']
-        if status != 'STOP' and order_id != 0 and sku_code != 'None' :
-            if standby_time>0:
-                standby_time-=1
-                acc_standby_time+=1
-                sql='update manufacturing_line set standby_time=%s,acc_standby_time=%s,status="STANDBY" where id=%s'%(standby_time,acc_standby_time,line_id)
+        target = row['target']
+        sku_code = row['sku_code']
+        if status != 'STOP' and order_id != 0 and sku_code != 'None':
+            if standby_time > 0:
+                standby_time -= 1
+                acc_standby_time += 1
+                sql = 'update manufacturing_line set standby_time=%s,acc_standby_time=%s,status="STANDBY" where id=%s' % (
+                    standby_time, acc_standby_time, line_id)
                 db_query(sql)
             else:
-                if setup_time>0:
-                    setup_time-=1
-                    acc_setup_time+=1
-                    sql='update manufacturing_line set setup_time=%s,acc_setup_time=%s,status="SETUP" where id=%s'%(setup_time,acc_setup_time,line_id)
+                if setup_time > 0:
+                    setup_time -= 1
+                    acc_setup_time += 1
+                    sql = 'update manufacturing_line set setup_time=%s,acc_setup_time=%s,status="SETUP" where id=%s' % (
+                        setup_time, acc_setup_time, line_id)
                     db_query(sql)
                 else:
-                    if temp_time<cycle_time:
-                        run_time+=1
-                        sql='update manufacturing_line set run_time=%s,status="RUNNING",remark="",location="" where id=%s'%(run_time,line_id)
+                    if temp_time < cycle_time:
+                        run_time += 1
+                        sql = 'update manufacturing_line set run_time=%s,status="RUNNING",remark="",location="" where id=%s' % (
+                            run_time, line_id)
                         db_query(sql)
-                    elif temp_time>=cycle_time:
-                        down_time+=1
+                    elif temp_time >= cycle_time:
+                        down_time += 1
                         # sql='update manufacturing_line set down_time=%s,status="BREAKDOWN",remark="" where id=%s'%(down_time,line_id)
                         # db_query(sql)
-                        if (temp_time-cycle_time) < small_stop_time:
-                            sql='update manufacturing_line set down_time=%s,status="SMALL STOP",remark="" where id=%s'%(down_time,line_id)
+                        if (temp_time - cycle_time) < small_stop_time:
+                            sql = 'update manufacturing_line set down_time=%s,status="SMALL STOP",remark="" where id=%s' % (
+                                down_time, line_id)
                             db_query(sql)
-                        elif (temp_time-cycle_time) >= small_stop_time:
-                            #tinggal ganti breakdown ke Down Time
-                            sql='update manufacturing_line set down_time=%s,status="DOWN TIME",remark="" where id=%s'%(down_time,line_id)
+                        elif (temp_time - cycle_time) >= small_stop_time:
+                            # tinggal ganti breakdown ke Down Time
+                            sql = 'update manufacturing_line set down_time=%s,status="DOWN TIME",remark="" where id=%s' % (
+                                down_time, line_id)
                             db_query(sql)
-                    temp_time+=1
-                    sql='update manufacturing_line set temp_time=%s where id=%s'%(temp_time,line_id)
+                    temp_time += 1
+                    sql = 'update manufacturing_line set temp_time=%s where id=%s' % (temp_time, line_id)
                     db_query(sql)
-            if item_counter!=prev_item_counter:
-                temp_time=0
-                delta_item_counter=item_counter-prev_item_counter
-                prev_item_counter=item_counter
-                sql='update manufacturing_line set temp_time=%s,prev_item_counter=%s,standby_time=0,setup_time=0,status="RUNNING" where id=%s'%(temp_time,prev_item_counter,line_id)
+            if item_counter != prev_item_counter:
+                temp_time = 0
+                delta_item_counter = item_counter - prev_item_counter
+                prev_item_counter = item_counter
+                sql = 'update manufacturing_line set temp_time=%s,prev_item_counter=%s,standby_time=0,setup_time=0,status="RUNNING" where id=%s' % (
+                    temp_time, prev_item_counter, line_id)
                 db_query(sql)
         else:
-            sql='update manufacturing_line set status="STOP" where id=%s'%line_id
+            sql = 'update manufacturing_line set status="STOP" where id=%s' % line_id
             db_query(sql)
-        availability=round((run_time*100/(run_time+down_time)) if (run_time+down_time)!=0 else 0,2)
-        performance=round(((cycle_time*item_counter)*100/(run_time+down_time)) if run_time!=0 else 0,2)
-        quality=round(((item_counter-ng_count)*100/item_counter) if item_counter!=0 else 0,2)
-        progress=round(((item_counter-ng_count)*100/target) if target!=0 else 0,2)
-        sql='update manufacturing_line set performance=%s,availability=%s,quality=%s,progress=%s where id=%s'%(performance,availability,quality,progress,line_id)
+        availability = round((run_time * 100 / (run_time + down_time)) if (run_time + down_time) != 0 else 0, 2)
+        performance = round(((cycle_time * item_counter) * 100 / (run_time + down_time)) if run_time != 0 else 0, 2)
+        quality = round(((item_counter - ng_count) * 100 / item_counter) if item_counter != 0 else 0, 2)
+        progress = round(((item_counter - ng_count) * 100 / target) if target != 0 else 0, 2)
+        sql = 'update manufacturing_line set performance=%s,availability=%s,quality=%s,progress=%s where id=%s' % (
+            performance, availability, quality, progress, line_id)
         db_query(sql)
         prev_log = db_fetchone(
             'select * from log_oee where line_name="%s" order by timestamp desc limit 1' % line_name)
@@ -111,8 +120,48 @@ def oee():
         if prev_status != status:
             # insert
             print('[MYSQL] Inserting log.')
-            sql='insert into log_oee (order_id,batch_id,lot_number,line_name,sku_code,item_counter,NG_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status) values(%s,"%s","%s","%s","%s",%s,%s,"%s",%s,%s,%s,%s,%s,"%s",%s,%s,"%s","%s")'%(order_id,batch_id,lot_number,line_name,sku_code,item_counter,ng_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status)
+            sql = 'insert into log_oee (order_id,batch_id,lot_number,line_name,sku_code,item_counter,NG_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status) values(%s,"%s","%s","%s","%s",%s,%s,"%s",%s,%s,%s,%s,%s,"%s",%s,%s,"%s","%s")' % (
+                order_id, batch_id, lot_number, line_name, sku_code, item_counter, ng_count, status, performance,
+                availability, quality, run_time, down_time, remark, acc_setup_time, acc_standby_time, location,
+                prev_status)
             db_query(sql)
+            if prev_status == 'SMALL STOP' and status == 'RUNNING':
+                # get latest small stop from this line
+                small_stop_log = db_fetchone(
+                    'select * from log_oee where line_name="%s" and status="SMALL STOP" and prev_status="RUNNING" order by timestamp desc limit 1' % line_name)
+                delta_downtime = down_time - small_stop_log['down_time']
+                print('calculating delta downtime')
+                if small_stop_log:
+                    sql = 'update log_oee set delta_down_time=%s where id=%s' % (delta_downtime, small_stop_log['id'])
+                    db_query(sql)
+                    print('updating delta downtime')
+                # selisih downtime
+                # update selisih downtime to log
+            elif prev_status == 'DOWN TIME' and status == 'RUNNING':
+                # get latest small stop from this line
+                small_stop_log = db_fetchone(
+                    'select * from log_oee where line_name="%s" and status="DOWN TIME" and prev_status="SMALL STOP" order by timestamp desc limit 1' % line_name)
+                delta_downtime = down_time - small_stop_log['down_time']
+                print('calculating delta downtime')
+                if small_stop_log:
+                    sql = 'update log_oee set delta_down_time=%s where id=%s' % (delta_downtime, small_stop_log['id'])
+                    db_query(sql)
+                    print('updating delta downtime')
+                # selisih downtime
+                # update selisih downtime to log
+            elif prev_status == 'SMALL STOP' and status == 'DOWN TIME':
+                # get latest small stop from this line
+                small_stop_log = db_fetchone(
+                    'select * from log_oee where line_name="%s" and status="SMALL STOP" and prev_status="RUNNING" order by timestamp desc limit 1' % line_name)
+                delta_downtime = down_time - small_stop_log['down_time']
+                print('calculating delta downtime')
+                if small_stop_log:
+                    sql = 'update log_oee set delta_down_time=%s where id=%s' % (delta_downtime, small_stop_log['id'])
+                    db_query(sql)
+                    print('updating delta downtime')
+                # selisih downtime
+                # update selisih downtime to log
+
         # sql='update order_list set performance=%s,availability=%s,quality=%s,progress=%s,NG_count=%s,item_counter=%s where id=%s'%(performance,availability,quality,progress,ng_count,item_counter,order_id)
         # db_query(sql)
         # print("setup time : %s"%row['setup_time'])
@@ -124,38 +173,43 @@ def oee():
         # print("acc standby time : %s"%row['acc_standby_time'])
         # print('--------------')
 
+
 def cronjob():
-    line=db_fetch('select * from manufacturing_line')
+    line = db_fetch('select * from manufacturing_line')
     # prev_log=db_fetchone('SELECT * FROM log_oee ORDER BY timestamp DESC')
     for row in line:
-        line_name=row['line_name']
-        order_id=row['order_id']
-        batch_id=row['batch_id']
-        lot_number=row['lot_number']
-        sku_code=row['sku_code']
-        status=row['status']
-        run_time=row['run_time']
-        down_time=row['down_time']
-        ng_count=row['NG_count']
-        item_counter=row['item_counter']
-        performance=row['performance']
-        availability=row['availability']
-        quality=row['quality']
-        remark=row['remark']
-        acc_standby_time=row['acc_standby_time']
-        acc_setup_time=row['acc_setup_time']
-        location=row['location']
+        line_name = row['line_name']
+        order_id = row['order_id']
+        batch_id = row['batch_id']
+        lot_number = row['lot_number']
+        sku_code = row['sku_code']
+        status = row['status']
+        run_time = row['run_time']
+        down_time = row['down_time']
+        ng_count = row['NG_count']
+        item_counter = row['item_counter']
+        performance = row['performance']
+        availability = row['availability']
+        quality = row['quality']
+        remark = row['remark']
+        acc_standby_time = row['acc_standby_time']
+        acc_setup_time = row['acc_setup_time']
+        location = row['location']
         prev_log = db_fetchone(
             'select * from log_oee where line_name="%s" order by timestamp desc limit 1' % line_name)
         prev_status = prev_log['status'] if prev_log else "STOP"
         # prev_status=prev_log['status']
-        sql='insert into log_oee (order_id,batch_id,lot_number,line_name,sku_code,item_counter,NG_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status) values(%s,"%s","%s","%s","%s",%s,%s,"%s",%s,%s,%s,%s,%s,"%s",%s,%s,"%s","%s")'%(order_id,batch_id,lot_number,line_name,sku_code,item_counter,ng_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status)
+        sql = 'insert into log_oee (order_id,batch_id,lot_number,line_name,sku_code,item_counter,NG_count,status,performance,availability,quality,run_time,down_time,remark,acc_setup_time,acc_standby_time,location,prev_status) values(%s,"%s","%s","%s","%s",%s,%s,"%s",%s,%s,%s,%s,%s,"%s",%s,%s,"%s","%s")' % (
+            order_id, batch_id, lot_number, line_name, sku_code, item_counter, ng_count, status, performance,
+            availability,
+            quality, run_time, down_time, remark, acc_setup_time, acc_standby_time, location, prev_status)
         db_query(sql)
-        
+
 
 try:
-    db_connect('dbdemo.colinn.id','oee4','admin','adminiot',3307)
+    # db_connect('dbdemo.colinn.id','oee4','admin','adminiot',3307)
     # db_connect('localhost','oee4','root','iotdb123')
+    db_connect('localhost', 'oee4', 'admin', 'adminiot')
     previousTime = 0
     eventInterval = 1000
     schedule.every().minute.at(":00").do(cronjob)
@@ -169,28 +223,28 @@ try:
                 # if currentTime - previousTime >= eventInterval :
                 #     oee()
                 #     previousTime = currentTime
-                time.sleep(1/1000)
+                time.sleep(1 / 1000)
             except KeyboardInterrupt:
                 print('[PROGRAM] Closed')
                 exit()
-            except Exception as e: 
+            except Exception as e:
                 print(e)
-                time.sleep(1/1000000)
+                time.sleep(1 / 1000000)
             except:
                 time.sleep(1)
                 print('[PROGRAM] Something is wrong')
-                time.sleep(1/1000000)
+                time.sleep(1 / 1000000)
         else:
             db_reconnect()
 
 except KeyboardInterrupt:
     print('[PROGRAM] Closed')
     exit()
-except Exception as e: 
+except Exception as e:
     print(e)
-    time.sleep(1/1000000)
+    time.sleep(1 / 1000000)
 except:
     print('[PROGRAM] Something is wrong')
-    time.sleep(1/1000000)
+    time.sleep(1 / 1000000)
 finally:
     db_close()
